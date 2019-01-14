dfcb1bf4abb6247c50fa30226c2d34f73ec4e8d2
Meghea Iulian <iulian.meghea@gmail.com>
2019-01-14 11:19:19 +0200

    Fix element type check in _onTap (#67)
    * Fix element type check in _onTap
    
    * Fixes es6

--- cosmoz-data-nav.js
+++ cosmoz-data-nav.js
@@ -693,7 +693,7 @@
 				return;
 			}
 			let inBetween = path.slice(path.indexOf(selectEl)),
-				ancestorNav = inBetween.find(e => e.is && e.is === this.is),
+				ancestorNav = inBetween.find(e => e && e.constructor && e.constructor.is === this.constructor.is),
 				select;
 
 			if (ancestorNav !== this) {
